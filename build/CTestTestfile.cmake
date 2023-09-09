c19c994a4ba86768e4b3154a57ee6804ad61a2ef
vishnu-096 <vishnu096@gmail.com>
2023-09-09 16:16:29 -0700

    starting repo for simulating realsense on gazebo. Attached to a turtle bot

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/vlv/pointcloud_prjs/ws/src
+# Build directory: /home/vlv/pointcloud_prjs/ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("tbot_sim")
+subdirs("realsense_gazebo_plugin")
---
+++ build/realsense_gazebo_plugin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vlv/pointcloud_prjs/ws/src/realsense_gazebo_plugin
+# Build directory: /home/vlv/pointcloud_prjs/ws/build/realsense_gazebo_plugin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
